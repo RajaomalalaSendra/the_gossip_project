dd1ad80ba7ade5364c5a6cc73519d14284139f73
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-28 10:04:16 +0300

    post and get gossip

--- lib/controller.rb
+++ lib/controller.rb
@@ -3,7 +3,12 @@ class ApplicationController < Sinatra::Base
   get '/' do
 	erb :index
   end
-  get '/gossip/new' do
+
+  get '/gossips/new/' do
   	erb :new_gossip
+  end
+
+  post '/gossips/new/' do
+  	puts "This is just a sample for the post"
   end
 end
